4a0ff641c9355cfa600715b4837c4655295421d8
zrblid <cookt9100@g.comalisd.org>
2026-02-09 18:51:10 -0600

    Add gradle.bat for Gradle wrapper execution

---
+++ gradle.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET DIRNAME=%~dp0
+SET APP_HOME=%DIRNAME%
+IF DEFINED JAVA_HOME (
+  SET JAVACMD=%JAVA_HOME%\bin\java.exe
+) ELSE (
+  SET JAVACMD=java.exe
+)
+"%JAVACMD%" -Dorg.gradle.appname=gradlew -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
